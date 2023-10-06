8cac77ba10e04ec86ee4733ae404cb048cf0bd0e
EnverJ <Enlem1987@gmail.com>
2023-10-05 20:49:46 -0600

    read only

--- src/main/java/io/ezbrains/springbootjpaexaple/SpringbootjpaexapleApplication.java
+++ src/main/java/io/ezbrains/springbootjpaexaple/SpringbootjpaexapleApplication.java
@@ -3,14 +3,19 @@ package io.ezbrains.springbootjpaexaple;
 import io.ezbrains.springbootjpaexaple.models.Employee;
 import io.ezbrains.springbootjpaexaple.repository.EmployeeRepository;
 import jakarta.annotation.PostConstruct;
-import jakarta.persistence.*;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.Propagation;
 
+
+import java.sql.SQLException;
 import java.util.Optional;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class SpringbootjpaexapleApplication {
 
 //	@PersistenceUnit
@@ -19,15 +24,19 @@ public class SpringbootjpaexapleApplication {
 //	@PersistenceContext
 //	private EntityManager entityManager;
 
-	@Autowired
-	EmployeeRepository employeeRepository;
+    @Autowired
+    EmployeeRepository employeeRepository;
 
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootjpaexapleApplication.class, args);
-	}
-	@PostConstruct
-	public void start() {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootjpaexapleApplication.class, args);
+    }
+
+    @PostConstruct
+
+    //read only transaction
+ //   @Transactional( readOnly = true)
+    public void start() {
 //		Employee employee = new Employee();
 //		employee.setAge(20);
 //		employee.setName("Foo Bar");
@@ -42,18 +51,37 @@ public class SpringbootjpaexapleApplication {
 //		entityManager.close();
 
 
-		Optional<Employee> employee = employeeRepository.findById(1);  // may or may not exist
-		if(employee.isPresent()) {
-			System.out.println(employee.get());
-			updateEmployee(employee.get());
-		}
+        Optional<Employee> employee = employeeRepository.findById(1);  // may or may not exist
+        if (employee.isPresent()) {
+            System.out.println(employee.get());
+            updateEmployee(employee.get());
+        }
 
-	}
+    }
+//@Transactional
+//    private void updateEmployeeAndAccessCard() {
+//      updateEmployee(e);
+//        accessCardRepository.save(a);
+//    }
 
-	private void updateEmployee(Employee employee) {
-		employee.setName("updated name");
-		employeeRepository.save(employee);
+  //  @Transactional(propagation = Propagation.REQUIRES_NEW)
+    @Transactional( rollbackOn = SQLException.class)
+//     @Transactional( dontRollbackOn = MyAppException.class)
+    private void updateEmployee(Employee employee) {
+        //GET transaction
+        //start transaction
+        employee.setName("updated name");
+        employeeRepository.save(employee);
+        //end transaction
+        //handle rollback
 
-	}
+
+    }
 
 }
+
+
+// Next step
+/* Read Documentation
+        . Build an application
+        . Watch the springboot courses. */
